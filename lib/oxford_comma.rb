095e9440e6589d3836ca88e7cd257116abc01ef1
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-08-14 22:36:04 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  l = array.length
+  if l == 1
+    return array.join
+  elsif l == 2 
+    return array[-2..-1].join(" and ")
+  elsif l >= 3
+    array.insert(-2, 'and') 
+    end
+  array
 end
+  #else if 
+  #end
+  #if 
+  #n = array[0..-2].join(", ")
